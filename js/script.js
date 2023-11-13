0689f33c1db896ea5ec851b6fa5957a6dbc63267
Laura Jaimes <146447166+geolaurajaimes@users.noreply.github.com>
2023-11-12 20:53:12 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let slideIndex = 0;
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    
+    slideIndex++;
+    
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change 2000 to the number of milliseconds you desire between each image change
+}
+
+document.addEventListener("DOMContentLoaded", showSlides);
