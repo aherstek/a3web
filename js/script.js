bd3ed5d8938e5e576891a7a0bf1b9fced5da3bb7
catamaralina <tali.jonker@gmail.com>
2023-11-13 20:23:00 -0500

    Dropdown menu js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
+/*For slideshow*/
 let slideIndex = 0;
-
 function showSlides() {
     let i;
     const slides = document.getElementsByClassName("mySlides");
@@ -19,3 +19,27 @@ function showSlides() {
 }
 
 document.addEventListener("DOMContentLoaded", showSlides);
+
+
+
+/*For the drop down menu*/
+/* following along with https://www.geeksforgeeks.org/how-to-create-dropdown-list-using-javascript/ */
+function show_container() {
+    var container = document.getElementById("projects"); 
+    /*changing container values on click: css .container{display = none;}  */
+    if (container.style.display === "block") {
+      container.style.display = "none";
+    } 
+    else {
+      container.style.display = "block";
+    }
+    
+    /*This is for if we don't take them to a new window*/
+    window.onclick = function (event) { /*When someone clicks on the window*/
+      if (!event.target.matches('.dropdown_button')) /*if someone clicks on the button*/
+        document.getElementsByClassName('') /*get elements listed as projects*/
+          .style.display = "block";
+  
+    }
+  }
+  
